5cbdcda495b765f7081c7edcf180b50f32c3b619
Christopher Fujino <christopherfujino@gmail.com>
2017-07-11 15:30:52 -0700

    added: getRelationship to compare two nodes and find their relationship

--- Matrix.js
+++ Matrix.js
@@ -64,3 +64,17 @@ Matrix.prototype.populateNeighbors = function () {
 Matrix.prototype.getRandomNode = function () {
   return this.grid[rand(this.height)][rand(this.width)];
 }
+
+Matrix.prototype.getRelationship = function (node1, node2) {
+  if (node1.neighbors.north === node2) {
+    return 'north';
+  } else if (node1.neighbors.east === node2) {
+    return 'east';
+  } else if (node1.neighbors.south === node2) {
+    return 'south';
+  } else if (node1.neighbors.west === node2) {
+    return 'west';
+  } else {
+    throw new Error('Huh?!', node1, node2);
+  }
+}
