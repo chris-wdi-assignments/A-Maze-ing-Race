24e8482c85efc9980444fa28bab99539e5beca4f
Christopher Fujino <christopherfujino@gmail.com>
2017-07-11 14:09:05 -0700

    added: Matrix now manages Node state more

--- Matrix.js
+++ Matrix.js
@@ -1,10 +1,27 @@
 const Matrix = function (width, height) {
   this.grid = []; // 2d matrix
+  this.buildMatrix(); // build matrix
+  this.allNodesSerialized = []; // all nodes in matrix will be pushed into one array
+                                // remember objects are stored in vars as references
+  this.grid.forEach(function (row) {
+    row.forEach(function (node) {
+      this.allNodesSerialized.push(node);
+    });
+  }); // .allNodesSerialized should now be populated
+  return arr;
+  this.populateNeighbors(); // link each node to its neighbors
+}
+
+Matrix.prototype.getRows = function () {
+  return this.grid; // array of row arrays
+}
+
+Matrix.prototype.buildMatrix = function () {
   for (let i = 0; i < height; i++) { // first do rows, to match DOM
     let row = [];
     for (let j = 0; j < width; j++) {
       row.push({
-        node: new Node(),
+        node: new Node(i, j),
         wasChecked: false
       });
     }
@@ -12,6 +29,9 @@ const Matrix = function (width, height) {
   }
 }
 
-Matrix.prototype.getRows = function () {
-  return this.grid; // array of row arrays
+Matrix.prototype.populateNeighbors = function () {
+  this.allNodesSerialized.forEach(function (node) { // made this arr so we
+                                                    // could do this!
+    //
+  });
 }
