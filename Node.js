eb12e886dad6283fda5dc62251bd8e9bba4cb28c
Christopher Fujino <christopherfujino@gmail.com>
2017-07-11 14:08:13 -0700

    added: `Node` now tracks its coordinates & links to its neighbors

--- Node.js
+++ Node.js
@@ -1,9 +1,21 @@
-const Node = function () {
+const Node = function (row, col) {
+  this.row = row; // this is our first coordinate, though not x
+  this.col = col; // matrix set up to model DOM, thus y, x
   this.walls = {
     north: false,
     east: false,
     south: false,
     west: false
   };
+  this.neighbors = {};  // must be initialized by .setNeighbors()
   this.$el = null;  // later bind DOM element to this node
 }
+
+Node.prototype.setNeighbors = function (north, east, south, west) {
+  this.neighbors = {
+    north: north,
+    east: east,
+    south: south,
+    west: west
+  };
+};
